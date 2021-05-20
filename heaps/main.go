59762e95d8a9f6f39fb03250a0805dcdca2570d7
Tim Garrity <tag.garrity@gmail.com>
2021-05-20 18:39:50 -0400

    heap redo

---
+++ heaps/main.go
@@ -0,0 +1,100 @@
+package main
+
+import "fmt"
+
+// MaxHeap struct has a slice that holds the array
+type MaxHeap struct {
+	array []int
+}
+
+// Insert
+func (h *MaxHeap) Insert(key int) {
+	h.array = append(h.array, key)
+	h.maxHeapifyUp(len(h.array) - 1)
+}
+
+// Extract
+func (h *MaxHeap) Extract() int {
+	extracted := h.array[0]
+	l := len(h.array) - 1
+
+	if len(h.array) == 0 {
+		fmt.Println("cannot extract because array length is 0")
+		return -1
+	}
+
+	h.array[0] = h.array[l]
+
+	h.array = h.array[:l]
+
+	h.maxHeapifyDown(0)
+
+	return extracted
+}
+
+// maxHeapifyUp will Heapify from bottom top
+func (h *MaxHeap) maxHeapifyUp(index int) {
+	for h.array[parent(index)] < h.array[index] {
+		h.swap(parent(index), index)
+		index = parent(index)
+	}
+}
+
+// parent get the route of the heap
+func parent(i int) int {
+	return (i - 1) / 2
+}
+
+// left finds the left child of a given key
+func left(i int) int {
+	return 2*i + 1
+}
+
+// right finds the right child of a given key
+func right(i int) int {
+	return 2*i + 2
+}
+
+func (h *MaxHeap) swap(i1, i2 int) {
+	h.array[i1], h.array[i2] = h.array[i2], h.array[i1]
+}
+
+func (h *MaxHeap) maxHeapifyDown(index int) {
+	lastIndex := len(h.array) - 1
+	l, r := left(index), right(index)
+	childToCompare := 0
+
+	for l <= lastIndex {
+		if l == lastIndex {
+			childToCompare = l
+		} else if h.array[l] > h.array[r] {
+			childToCompare = l
+		} else {
+			childToCompare = r
+		}
+
+		if h.array[index] < h.array[childToCompare] {
+			h.swap(index, childToCompare)
+			index = childToCompare
+			l, r = left(index), right(index)
+		} else {
+			return
+		}
+	}
+}
+
+func main() {
+	m := &MaxHeap{}
+	fmt.Println(m)
+	buildHeap := []int{20, 30, 40, 5, 7, 9, 11, 14, 15, 17}
+
+	for _, v := range buildHeap {
+		m.Insert(v)
+		fmt.Println(m)
+	}
+
+	for i := 0; i < 5; i++ {
+		m.Extract()
+		fmt.Println(m)
+	}
+}
